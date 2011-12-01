0c862acb36f90c80799f22fed1acfe7c7ddd7c28
azu <azuciao@gmail.com>
2011-12-01 13:23:25 +0900

    Initial Commit

---
+++ NumericKeypad/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NumericKeypad
+//
+//  Created by  on 11/12/01.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NumericKeypad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NumericKeypad
+//
+//  Created by  on 11/12/01.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
